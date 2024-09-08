430b7314a2e6c071785179698f8fea60bfcbffbc
uygar <uygargurnal2@gmail.com>
2024-09-08 14:20:02 +0300

    ekle işler hale geldi

--- gui.py
+++ gui.py
@@ -1,10 +1,23 @@
+
 import functions as xd
 import FreeSimpleGUI as sg
 
 yazi = sg.Text("Kadroya kimi ekleyeceğini yaz.")
-gir = sg.InputText("Kimi ekleyecen")
+gir = sg.InputText(key="oyuncu")
 buton = sg.Button("Ekle")
 
-window = sg.Window("Kadrolama Programı",layout=[[yazi],[gir,buton]])
-window.read()
+window = sg.Window("Kadrolama Programı",layout=[[yazi],[gir,buton]], font=('Cuckoo',20))
+
+while True:
+    olay, isim = window.read()
+    print(olay)
+    print(isim)
+    match olay:
+        case "Ekle":
+            kadro = xd.dosya_ac()
+            yeni = isim["oyuncu"]+"\n"
+            kadro.append(yeni)
+            xd.dosya_yaz(kadro)
+        case sg.WINDOW_CLOSED:
+            break
 window.close()
