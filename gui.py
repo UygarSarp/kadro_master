816155b2be651794bdbf1d240009543957ac50f1
uygar <uygargurnal2@gmail.com>
2024-09-08 15:36:34 +0300

    değiştir, çıkar, sıfırla, çık eklendi

--- gui.py
+++ gui.py
@@ -3,10 +3,17 @@ import functions as xd
 import FreeSimpleGUI as sg
 
 yazi = sg.Text("Kadroya kimi ekleyeceğini yaz.")
-gir = sg.InputText(key="oyuncu")
+gir = sg.InputText(tooltip="Oyuncu gir",key="oyuncu")
 buton = sg.Button("Ekle")
+list_box = sg.Listbox(values=xd.dosya_ac(),key = "kadro", enable_events=True, size=(45,10))
+değiştir = sg.Button("Değiştir")
+baybay = sg.Button("Çıkar")
+destroy = sg.Button("Sıfırla")
+run = sg.Button("Çık")
 
-window = sg.Window("Kadrolama Programı",layout=[[yazi],[gir,buton]], font=('Cuckoo',20))
+window = sg.Window("Kadrolama Programı",
+                   layout=[[yazi],[gir,buton],[list_box,değiştir,baybay],[run,destroy]],
+                   font=('Cuckoo',20))
 
 while True:
     olay, isim = window.read()
@@ -18,6 +25,46 @@ while True:
             yeni = isim["oyuncu"]+"\n"
             kadro.append(yeni)
             xd.dosya_yaz(kadro)
+            window["kadro"].update(kadro)
+            window["oyuncu"].update("")
+        case "Değiştir":
+            try:
+                gidici = isim["kadro"][0]
+                gelici = isim["oyuncu"]+"\n"
+                kadro = xd.dosya_ac()
+                index = kadro.index(gidici)
+                kadro[index] = gelici
+                xd.dosya_yaz(kadro)
+                window["kadro"].update(kadro)
+            except IndexError:
+                error_yazi = sg.Text("Lütfen işlem yapacağınız oyuncunun üstüne tıklayın.")
+                error_buton = sg.Button("Tamam")
+                error = sg.Window("Error",layout=[[error_yazi],[error_buton]])
+                error.read()
+                error.close()
+        case "Çıkar":
+            gidici = isim["kadro"][0]
+            kadro = xd.dosya_ac()
+            index = kadro.index(gidici)
+            kadro.pop(index)
+            xd.dosya_yaz(kadro)
+            window["kadro"].update(kadro)
+
+        case "kadro":
+            try:
+                window["oyuncu"].update(isim["kadro"][0])
+            except IndexError:
+                error_yazi = sg.Text("Bir isim seçebilmek için listenizde en az bir isim olmalıdır!")
+                error_buton = sg.Button("Tamam")
+                error = sg.Window("Error", layout=[[error_yazi], [error_buton]])
+                error.read()
+                error.close()
+        case "Sıfırla":
+            xd.dosya_yaz("")
+            window["kadro"].update("")
+
+        case "Çık":
+            break
         case sg.WINDOW_CLOSED:
             break
 window.close()
